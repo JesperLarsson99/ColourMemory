005e2625a3dc6f1b76c4b70cac39753d7a1b23d6
Jesper Larsson <jesper.larsson1999@gmail.com>
2025-09-19 12:37:19 +0200

    Includes the gameplay of clicking cards

--- Colour Memory/GameplayHandler.cs
+++ Colour Memory/GameplayHandler.cs
@@ -1,23 +1,66 @@
 ﻿namespace Colour_Memory;
 internal class GameplayHandler(Form1 form)
 {
-    internal Dictionary<Button, Color> cardColors = new Dictionary<Button, Color>();
+    internal Dictionary<Button, Color> CardColors = new Dictionary<Button, Color>();
+    internal List<Button> ClickedCards = new List<Button>();
+
+    private bool allowedToClick = true;
 
     internal void StartGame()
     {
         var colors = GameSetup.SetupCardColors();
 
-        cardColors = GameSetup.MatchCardsWithColors(colors, form);
+        CardColors = GameSetup.MatchCardsWithColors(colors, form);
     }
 
     internal void OnCardClick(object? sender, EventArgs e)
     {
+        if (!allowedToClick)
+        {
+            return;
+        }
+
         var clickedCard = sender as Button;
 
         if (clickedCard != null)
         {
-            clickedCard.BackColor = cardColors[clickedCard];
+            clickedCard.BackColor = CardColors[clickedCard];
             clickedCard.Image = null;
+            clickedCard.Enabled = false;
+            ClickedCards.Add(clickedCard);
+        }
+
+        if (ClickedCards.Count == 2)
+        {
+            allowedToClick = false;
+            
+            var timer = new System.Windows.Forms.Timer();
+            timer.Interval = 2000;
+
+            timer.Tick += (s, args) =>
+            {
+                timer.Stop();
+
+                if (ClickedCards[0].BackColor == ClickedCards[1].BackColor)
+                {
+                    ClickedCards.ForEach(card => card.Visible = false);
+                }
+                else 
+                {
+                    ClickedCards.ForEach(card => card.Image = Properties.Resources.background);
+
+                    foreach (var card in ClickedCards)
+                    {
+                        card.Image = Properties.Resources.background;
+                        card.Enabled = true;
+                    }
+                }
+
+                ClickedCards.Clear();
+                allowedToClick = true;
+            };
+
+            timer.Start();
         }
     }
 }
